fbc6322d61ea70c7787b6fe51fa39562ce08b8d2
Dmitriy Kovalev <kodimweb@gmail.com>
2019-10-02 12:46:03 +0300

    added underscors theme and update plugin

---
+++ www/wp-content/themes/evgenstudio/inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * evgenstudio Theme Customizer
+ *
+ * @package evgenstudio
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function evgenstudio_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'evgenstudio_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'evgenstudio_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'evgenstudio_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function evgenstudio_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function evgenstudio_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function evgenstudio_customize_preview_js() {
+	wp_enqueue_script( 'evgenstudio-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'evgenstudio_customize_preview_js' );
