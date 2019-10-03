81c1c8767de3a3259fa50b80daff12466cb71414
Dmitriy Kovalev Home <kodimweb@gmail.com>
2019-10-03 23:18:17 +0300

    worked

--- www/wp-content/themes/evgenstudio/js/main.js
+++ www/wp-content/themes/evgenstudio/js/main.js
@@ -12,7 +12,20 @@ jQuery( document ).ready( function( $ ) {
 
     console.log($('.main-content').offset());
 
+    var customScroCount = 0;
     $(window).scroll(function(){
+        var customScrolltop = false;
+        
+        if (customScroCount < $(window).scrollTop() ) {
+            customScrolltop = false;
+            customScroCount = $(window).scrollTop();
+        } else {
+            customScrolltop = true;
+            customScroCount = $(window).scrollTop();
+        }
+
+        console.log(customScrolltop);
+        console.log(customScroCount);
         var block = document.querySelector('.site-background-right');
         if($(window).scrollTop() > $('.main-content').offset().top - 100){
             block.style.animationName = 'main-content-start';
@@ -21,8 +34,13 @@ jQuery( document ).ready( function( $ ) {
                 block.style.backgroundColor = '#2E3E43';
             }, 1500 );
         }
-        console.log($('.main-content').offset());
-        console.log($(window).scrollTop());
+
+        if($(window).scrollTop() < $('.main-content').offset().top - 100 && customScrolltop && block.style.animationName != 'main-content-start-back') {
+            block.style.animationName = 'main-content-start-back';
+            setTimeout(function(){
+                block.style.backgroundColor = '#01B8ED';
+            }, 1500 );
+        }
     });
 
 });
